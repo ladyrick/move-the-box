ecd9e8874a52c2bf979db676be899192058b3215
Hong Liu <liuhong12th@163.com>
2016-09-18 12:55:27 +0800

    删除一些没用的关于解谜的代码

--- movethebox.js
+++ movethebox.js
@@ -4,10 +4,7 @@ mb.state = [new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(
 mb.todo = [new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(7)];
 mb.boxToBeSwaped = [];
 mb.isMoving = false;
-mb.solution = [];
-mb.placeMode = true;
 mb.stepLimit = 0;
-mb.currentColor = "a";
 
 mb.swap = function (si, sj, ei, ej) {
     if (si < 0 || sj < 0 || ei < 0 || ej < 0 || si > 8 || sj > 6 || ei > 8 || ej > 6)
@@ -21,29 +18,6 @@ mb.swap = function (si, sj, ei, ej) {
     var temp = mb.state[si][sj];
     mb.state[si][sj] = mb.state[ei][ej];
     mb.state[ei][ej] = temp;
-};
-
-mb.placeBox = function (i, j) {
-    switch (event.button) {
-        case 0: {
-            document.getElementById('b' + i + j).className = mb.currentColor;
-            mb.state[i][j] = mb.currentColor.charCodeAt(0) - 96;
-            break;
-        }
-        case 1: {
-            document.getElementById('b' + i + j).removeAttribute("class");
-            mb.state[i][j] = 0;
-            break;
-        }
-        case 2: {
-            if (mb.state[i][j] === 0)
-                mb.currentColor = String.fromCharCode((mb.currentColor.charCodeAt(0) - 97 + 1) % 12 + 97);
-            else
-                mb.currentColor = document.getElementById('b' + i + j).className;
-            break;
-        }
-        default:
-    }
 };
 
 mb.isStable = function (state) {
@@ -105,148 +79,10 @@ mb.destoryBox = function () {
         }
 };
 
-mb.getSolution = function (state, steplimit) {
-    var solved = true;
-    for (var i = 0; i < 9; i++)
-        for (var j = 0; j < 7; j++)
-            if (state[i][j] !== 0)
-                solved = false;
-    if (solved)
-        return true;
-    else {
-        if (steplimit === 0)
-            return false;
-        var swap = function (i1, j1, i2, j2) {
-            if (i1 > 8 || j1 > 6 || i2 > 8 || j2 > 6 || state[i1][j1] === state[i2][j2])
-                return [];
-            var temp;
-            var tempstate = [];
-            state.forEach(function (t) {
-                tempstate.push(t.slice())
-            });
-            temp = tempstate[i1][j1];
-            tempstate[i1][j1] = tempstate[i2][j2];
-            tempstate[i2][j2] = temp;
-            return tempstate;
-        };
-        var todo = [new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(7), new Int8Array(7)];
-        for (i = 0; i < 9; i++)
-            for (j = 0; j < 7; j++) {
-                var d = [[1, 0], [0, 1]];
-                for (var k = 0; k < 2; k++) {
-                    var temp = swap(i, j, i + d[k][0], j + d[k][1]);
-                    if (temp.length === 0) {
-                        continue;
-                    }
-                    var finish = false;
-                    while (!finish) {
-                        finish = true;
-                        if (!mb.isStable(temp)) {
-                            (function () {
-                                for (var j = 0; j < 7; j++) {
-                                    var empthNum = 0;
-                                    for (var i = 0; i < 9; i++) {
-                                        if (temp[i][j] === 0)
-                                            empthNum++;
-                                        else if (empthNum > 0) {
-                                            temp[i - empthNum][j] = temp[i][j];
-                                            temp[i][j] = 0;
-                                        }
-                                    }
-                                }
-                            })();
-                            finish = false;
-                        }
-                        else if (!mb.check(temp, todo)) {
-                            (function () {
-                                for (var i = 0; i < 9; i++)
-                                    for (var j = 0; j < 7; j++) {
-                                        if (todo[i][j]) {
-                                            temp[i][j] = 0;
-                                            todo[i][j] = 0;
-                                        }
-                                    }
-                            })();
-                            finish = false;
-                        }
-                    }
-                    if (mb.getSolution(temp, steplimit - 1)) {
-                        mb.solution.push([i, j, i + d[k][0], j + d[k][1]]);
-                        return true;
-                    }
-                }
-            }
-    }
-    return false;
-};
-
 mb.onmousedownfunc = function (i, j) {
     if (mb.isMoving)
         return;
-    if (mb.placeMode)
-        mb.placeBox(i, j);
-    else
-        mb.swapByHand(i, j);
-};
-
-mb.autoSolve = function () {
-    if (mb.isMoving)
-        return "is moving.";
-    var solved = true;
-    for (var i = 0; i < 9; i++)
-        for (var j = 0; j < 7; j++)
-            if (mb.state[i][j] !== 0)
-                solved = false;
-    if (solved)
-        return "already solved";
-    for (i = 1; i < 5; i++) {
-        if (mb.getSolution(mb.state, i))
-            break;
-    }
-    if (i === 5)
-        return "unsolveable;";
-    else
-        mb.stepLimit = i;
-    mb.isMoving = true;
-    var statebackup = [];
-    mb.state.forEach(function (e) {
-        statebackup.push(e.slice());
-    });
-    var solutionbackup = [];
-    mb.solution.forEach(function (e) {
-        solutionbackup.push(e.slice());
-    });
-    document.getElementById("containbox").className = "wait";
-    var timeout = 350;
-    var l = mb.solution.length - 1;
-    var finish = true;
-    var moving = function () {
-        if (finish) {
-            mb.swap(mb.solution[l][0], mb.solution[l][1], mb.solution[l][2], mb.solution[l][3]);
-            l--;
-            mb.solution.pop();
-            finish = false;
-            setTimeout(moving, timeout);
-        }
-        else if (!mb.isStable(mb.state)) {
-            mb.fallDown();
-            setTimeout(moving, timeout);
-        }
-        else if (!mb.check(mb.state, mb.todo)) {
-            mb.destoryBox();
-            setTimeout(moving, timeout);
-        }
-        else if (l >= 0) {
-            finish = true;
-            setTimeout(moving, timeout);
-        }
-        else {
-            mb.isMoving = false;
-            document.getElementById("containbox").className = "pointer";
-        }
-    };
-    moving();
-    return [statebackup, solutionbackup];
+    mb.swapByHand(i, j);
 };
 
 mb.swapByHand = function (i, j) {
@@ -303,47 +139,6 @@ mb.swapByHand = function (i, j) {
         }
         e2.style.boxShadow = "0 0 3px 3px " + color;
         e2.style.zIndex = 1;
-    }
-};
-
-mb.addList = function () {
-    var r = mb.autoSolve();
-    mb.currentColor = 'a';
-    if (typeof r === "object") {
-        var state = r[0];
-        var solution = r[1];
-        var puzzle = "";
-        for (var i = 0; i < 9; i++) {
-            var c = state[i][0];
-            var n = 1;
-            for (var j = 1; j < 7; j++) {
-                if (state[i][j] === c) {
-                    n++;
-                }
-                else {
-                    c = "#abcdefghijkl"[c];
-                    puzzle += n === 1 ? c : '' + n + c;
-                    c = state[i][j];
-                    n = 1;
-                }
-            }
-            puzzle += '$';
-        }
-        for (i = puzzle.length - 1; i >= 0; i--) {
-            if (puzzle[i] === '$')
-                puzzle = puzzle.substring(0, i);
-            else
-                break;
-        }
-        puzzle += '!';
-        solution.reverse();
-        var s = "";
-        solution.forEach(function (e) {
-            s += "" + (e[0] + 1) + (e[1] + 1) + "-" + (e[2] + 1) + (e[3] + 1) + " ";
-        });
-        var newlist = document.createElement('p');
-        newlist.innerHTML = "<strong>" + mb.stepLimit + "</strong>" + puzzle + "<strong>" + s + "</strong>";
-        document.getElementById("list").appendChild(newlist);
     }
 };
 
